6e25076cf775e6946f35c21a947f81d15e9e1dbe
calypsomatic <calypsomatic@gmail.com>
2013-12-01 22:36:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moments::Application.config.secret_key_base = 'b5a7434f2dd1f22fdf1ce00f371cf783798cf10406659e01dec603e4fac0c167e7e4e99e15cac31a0113867a4f18d981a24fd0075844a5631a6a510439bb75b1'
